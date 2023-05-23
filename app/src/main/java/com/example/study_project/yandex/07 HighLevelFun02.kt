8a16ca5a1ee4e26e7c03b3b93db498da682f0bb4
ASUS <apostol_maxim@mail.ru>
2023-05-23 19:55:49 +0300

    update программы для студентов

--- app/src/main/java/com/example/study_project/yandex/07 HighLevelFun02.kt
+++ app/src/main/java/com/example/study_project/yandex/07 HighLevelFun02.kt
@@ -2,33 +2,21 @@ package com.example.study_project.yandex
 
 
 fun main() {
-    val action1 = selectAction(1)
-    println(action1(8, 5))    // 13
+    val action1 = selectAction()
+//    println(action1(8, 5))
 
-    val action2 = selectAction(2)
-    println(action2(8, 5))    // 3
+    val action2 = selectAction()
+//    println(action2(8, 5))
 
-    val action3 = selectAction(3)
-    println(action3(8, 5))    // 40
+    val action3 = selectAction()
+//    println(action3(8, 5))
 }
 
-/**
- * по ключу выбираем функцию
- */
-fun selectAction(key: Int): (Int, Int) -> Int =
-    // определение возвращаемого результата
-    when (key) {
-        // тут мы видим что явной передачи параметров нет
-        // и может быть не ясно откуда они берутся
-        //
-        1 -> ::sum
-        2 -> ::subtract
-        3 -> ::multiply
-        else -> ::empty
-    }
+fun selectAction(){
 
+}
 
-fun empty(a: Int, b: Int): Int {
+fun empty(a: Int = 0, b: Int = 0): Int {
     return 0
 }
 
@@ -47,3 +35,34 @@ fun multiply(a: Int, b: Int): Int {
 fun multiply2(a: Int): Int {
     return a * a
 }
+
+/*
+
+interface Action {
+    fun invoke(a: Int = 0, b: Int = 0): Int
+}
+
+fun main() {
+    val action1 = selectAction(1)
+    println(action1.invoke(8, 5))
+
+    val action2 = selectAction(2)
+    println(action2.invoke(8, 5))
+
+    val action3 = selectAction(3)
+    println(action3.invoke(8))
+
+    val action4 = selectAction(4)
+    println(action4.invoke())
+}
+
+fun selectAction(key: Int): Action =
+    when (key) {
+        1 -> object: Action { override fun invoke(a: Int, b: Int): Int = a + b }
+        2 -> object: Action { override fun invoke(a: Int, b: Int): Int = a - b }
+        3 -> object: Action { override fun invoke(a: Int, b: Int): Int = a * a } // Использует только первый аргумент
+        else -> object: Action { override fun invoke(a: Int, b: Int): Int = 0 } // Игнорирует все аргументы
+    }
+
+*/
+
