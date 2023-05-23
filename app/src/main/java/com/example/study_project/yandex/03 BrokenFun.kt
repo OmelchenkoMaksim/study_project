c2b9949d0aad847f3224d98725a4fecfd2165812
ASUS <apostol_maxim@mail.ru>
2023-05-23 18:01:58 +0300

    update программы для студентов

--- app/src/main/java/com/example/study_project/yandex/03 BrokenFun.kt
+++ app/src/main/java/com/example/study_project/yandex/03 BrokenFun.kt
@@ -1,11 +1,9 @@
 package com.example.study_project.yandex
 
-import android.app.Activity
-
 // починить эту функцию и сделать расширением для активити
 
-inline fun <reified T : java.io.Serializable?> Activity.getSerializable(name: String) =
-    intent.getSerializableExtra(name) as T
-
+fun <T : java.io.Serializable?> getSerializable(name: String) {
+    return intent.getSerializableExtra(name) as T
+}
 
 
